85f0a83b0925e72482d877fa27b2cd899307e3e8
lyp <luoyiping@hengzhongvip.com>
2018-12-24 15:57:37 +0800

    ''

--- src/main/java/com/hz/shiro/ShiroConfiguration.java
+++ src/main/java/com/hz/shiro/ShiroConfiguration.java
@@ -4,6 +4,7 @@ import com.hz.shiro.shiroDomain.ShiroProperties;
 import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.RememberMeManager;
 import org.apache.shiro.realm.Realm;
@@ -11,7 +12,9 @@ import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -56,16 +59,16 @@ public class ShiroConfiguration {
 	}
 
 	 
-	 /**
-     * (基于内存的)用户授权信息Cache
-     */
-    @Bean(name = "shiroCacheManager")
-    @ConditionalOnMissingBean(name = "shiroCacheManager")
-    @ConditionalOnMissingClass(value = {"org.apache.shiro.cache.ehcache.EhCacheManager"})
-    public CacheManager memoryCacheManager() {
-
-    	return new MemoryConstrainedCacheManager();
-    }
+//	 /**
+//     * (基于内存的)用户授权信息Cache
+//     */
+//    @Bean(name = "shiroCacheManager")
+//    @ConditionalOnMissingBean(name = "shiroCacheManager")
+//    @ConditionalOnMissingClass(value = {"org.apache.shiro.cache.ehcache.EhCacheManager"})
+//    public CacheManager memoryCacheManager() {
+//
+//    	return new MemoryConstrainedCacheManager();
+//    }
     
 
     /**
@@ -78,20 +81,20 @@ public class ShiroConfiguration {
         return new RedisCacheManager(redisTemplate);
     }
     
-    /**
-     * (基于ehcache的)用户授权信息Cache
-     */
-    @Bean(name = "shiroCacheManager")
-    @ConditionalOnClass(name = {"org.apache.shiro.cache.ehcache.EhCacheManager"})
-    @ConditionalOnMissingBean(name = "shiroCacheManager")
-    public CacheManager ehcacheCacheManager() {
-        EhCacheManager ehCacheManager = new EhCacheManager();
-        ShiroProperties.Ehcache ehcache = properties.getEhcache();
-        if (ehcache.getCacheManagerConfigFile() != null) {
-            ehCacheManager.setCacheManagerConfigFile(ehcache.getCacheManagerConfigFile());
-        }
-        return ehCacheManager;
-    }
+//    /**
+//     * (基于ehcache的)用户授权信息Cache
+//     */
+//    @Bean(name = "shiroCacheManager")
+//    @ConditionalOnClass(name = {"org.apache.shiro.cache.ehcache.EhCacheManager"})
+//    @ConditionalOnMissingBean(name = "shiroCacheManager")
+//    public CacheManager ehcacheCacheManager() {
+//        EhCacheManager ehCacheManager = new EhCacheManager();
+//        ShiroProperties.Ehcache ehcache = properties.getEhcache();
+//        if (ehcache.getCacheManagerConfigFile() != null) {
+//            ehCacheManager.setCacheManagerConfigFile(ehcache.getCacheManagerConfigFile());
+//        }
+//        return ehCacheManager;
+//    }
 
 
 	@Bean(name = "lifecycleBeanPostProcessor")
@@ -120,6 +123,29 @@ public class ShiroConfiguration {
 		sm.setRememberMeManager(rememberMeManager);
 		sm.setRealm(realm);
 		return sm;
+	}
+	/**
+	 * cookie管理对象;
+	 * rememberMeManager()方法是生成rememberMe管理器，而且要将这个rememberMe管理器设置到securityManager中
+	 * @return
+	 */
+	@Bean
+	public CookieRememberMeManager rememberMeManager(){
+		//System.out.println("ShiroConfiguration.rememberMeManager()");
+		CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+		cookieRememberMeManager.setCookie(rememberMeCookie());
+		//rememberMe cookie加密的密钥 建议每个项目都不一样 默认AES算法 密钥长度(128 256 512 位)
+		cookieRememberMeManager.setCipherKey(Base64.decode("2AvVhdsgUs0FSA3SDFAdag=="));
+		return cookieRememberMeManager;
+	}
+	@Bean
+	public SimpleCookie rememberMeCookie(){
+		//System.out.println("ShiroConfiguration.rememberMeCookie()");
+		//这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
+		SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+		//<!-- 记住我cookie生效时间30天 ,单位秒;-->
+		simpleCookie.setMaxAge(259200);
+		return simpleCookie;
 	}
 
 	@Bean
